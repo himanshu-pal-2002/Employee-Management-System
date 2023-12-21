cc08a8ee838912bd0a1ed52c741b229513c75c81
Himanshu Pal <palhimanshu206243@gmail.com>
2023-12-21 23:31:02 +0530

    employee management

--- emp/urls.py
+++ emp/urls.py
@@ -3,7 +3,8 @@ from emp.views import *
 
 urlpatterns=[
     path('',index,name='index'),
-    path('',index,name='index'),
-    path('',index,name='index'),
-    path('',index,name='index'),
+    path('all_emp/',all_emp,name='all_emp'),
+    path('add_emp/',add_emp,name='add_emp'),
+    path('remove_emp/',remove_emp,name='remove_emp'),
+    path('filter_emp/',filter_emp,name='filter_emp'),
 ]
--- emp/views.py
+++ emp/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import Employee,Role,Department
 
 # Create your views here.
 def index(request):
     return render(request,'emp/index.html')
+
+def all_emp(request):
+    emps=Employee.objects.all()
+    context = {
+        'emps':emps
+    }
+    return render(request,'emp/view_all_emp.html',context)
+
+def add_emp(request):
+    return render(request,'emp/add_emp.html')
+
+def remove_emp(request):
+    return render(request,'emp/remove_emp.html')
+
+def filter_emp(request):
+    return render(request,'emp/filter_emp.html')
