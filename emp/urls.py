4a65f0176f858ea73552d77bd72353bce7f75001
Himanshu Pal <palhimanshu206243@gmail.com>
2023-12-22 23:27:44 +0530

    employeemanagement

--- emp/urls.py
+++ emp/urls.py
@@ -3,8 +3,9 @@ from emp.views import *
 
 urlpatterns=[
     path('',index,name='index'),
-    path('all_emp/',all_emp,name='all_emp'),
-    path('add_emp/',add_emp,name='add_emp'),
-    path('remove_emp/',remove_emp,name='remove_emp'),
-    path('filter_emp/',filter_emp,name='filter_emp'),
+    path('All_Emp',All_Emp,name='All_Emp'),
+    path('Add_Emp',Add_Emp,name='Add_Emp'),
+    path('Remove_Emp',Remove_Emp,name='Remove_Emp'),
+    path('Remove_Emp/<int:emp_id',Remove_Emp,name='Remove_Emp'),
+    path('Filter_Emp',Filter_Emp,name='Filter_Emp'),
 ]
--- emp/views.py
+++ emp/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Employee,Role,Department
 
@@ -5,18 +6,47 @@ from .models import Employee,Role,Department
 def index(request):
     return render(request,'emp/index.html')
 
-def all_emp(request):
+def All_Emp(request):
     emps=Employee.objects.all()
     context = {
         'emps':emps
     }
     return render(request,'emp/view_all_emp.html',context)
 
-def add_emp(request):
-    return render(request,'emp/add_emp.html')
+def Add_Emp(request):
+    if request.method == 'POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        dept=int(request.POST['dept'])
+        salary=int(request.POST['salary'])
+        bonus=int(request.POST['bonus'])
+        role=int(request.POST['role'])
+        phone=int(request.POST['phone'])
+        hire_date=request.POST['hire_date']
+        new_emp=Employee(first_name=first_name,last_name=last_name,dept_id=dept,salary=salary,bonus=bonus,role_id=role,
+                         phone=phone,hire_date=hire_date)
+        new_emp.save()
+        return HttpResponse('Employee added Successfully')
+        
+    elif request.method=='GET':
+        return render(request,'emp/add_emp.html')
+    
+    else:
+        return HttpResponse('An exception occured Employee Has not added')
 
-def remove_emp(request):
-    return render(request,'emp/remove_emp.html')
+def Remove_Emp(request,emp_id=0):
+    if emp_id:
+        try:
+            emp_to_be_removed=Employee.objects.get(id=emp_id)
+            emp_to_be_removed.delete()
+            return HttpResponse('Employee Removed Successfully')
+        except:
+            return HttpResponse('Please Enter a Valid Emp Id')
+    emps=Employee.objects.all()
+    context={
+        'emps':emps
+    }
+    return render(request,'emp/remove_emp.html',context)
 
-def filter_emp(request):
+def Filter_Emp(request):
     return render(request,'emp/filter_emp.html')
